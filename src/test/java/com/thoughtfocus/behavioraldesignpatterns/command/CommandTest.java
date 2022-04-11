ce96cca71ca99df7310ec4d3b2b33b28a79a0bfe
106580 <saisunilkm>
2022-04-11 18:19:07 +0530

    modifications done

--- src/test/java/com/thoughtfocus/behavioraldesignpatterns/command/CommandTest.java
+++ src/test/java/com/thoughtfocus/behavioraldesignpatterns/command/CommandTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CommandTest {
     @Test
-    public void whenRemoteSetToTurnOnLight_AndButtonPressed_LightShouldTurnOn() {
+    public void whenRemoteSetToTurnOnLight_AndButtonPressed_LightShouldTurnOn_LightShouldTurnOff() {
 
         Light livingRoomLight = new Light();
 
@@ -15,22 +15,14 @@ public class CommandTest {
         remote.setCommand(new TurnOnLightCommand(livingRoomLight));
         assertEquals("Turning on light.Light is On", remote.buttonPressed());
 
-    }
-
-    @Test
-    public void whenRemoteSetToTurnOffLight_AndButtonPressed_LightShouldTurnOff() {
-
-        Light livingRoomLight = new Light();
-
-        HomeAutomationRemote remote = new HomeAutomationRemote();
-
         remote.setCommand(new TurnOffLightCommand(livingRoomLight));
         assertEquals("Turning off light.Light is Off", remote.buttonPressed());
 
     }
 
+
     @Test
-    public void whenRemoteSetToStartFan_AndButtonPressed_FanShouldStart() {
+    public void whenRemoteSetToStartFan_AndButtonPressed_FanShouldStart_FanShouldStop() {
 
         Fan livingRoomFan = new Fan();
 
@@ -38,15 +30,6 @@ public class CommandTest {
 
         remote.setCommand(new StartFanCommand(livingRoomFan));
         assertEquals("Starting fan...Fan started...", remote.buttonPressed());
-
-    }
-
-    @Test
-    public void whenRemoteSetToStopFan_AndButtonPressed_FanShouldStop() {
-
-        Fan livingRoomFan = new Fan();
-
-        HomeAutomationRemote remote = new HomeAutomationRemote();
 
         remote.setCommand(new StopFanCommand(livingRoomFan));
         assertEquals("Stopping fan...Fan stopped...", remote.buttonPressed());
